4380228c8002bf0668579d4bf48eae31ef672040
Mario Roberto <gomez@thetecwave.com>
2025-08-05 21:57:11 -0600

    fix: Correct all schema imports in __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,22 +1,22 @@
 from .user import UserCreate, UserUpdate, UserResponse, UserLogin, Token
-from .product import Product, ProductCreate, ProductUpdate, ProductResponse, ProductList, ProductWithStats
-from .commentary import Commentary, CommentaryCreate, CommentaryUpdate, CommentaryResponse
-from .like import Like, LikeCreate, LikeResponse
-from .event import Event, EventCreate, EventUpdate, EventResponse
-from .waiver import WaiverData, WaiverDataCreate, WaiverDataResponse, WaiverValidator, WaiverValidatorCreate, WaiverValidatorResponse
-from .chat import ChatRoom, ChatRoomCreate, ChatRoomResponse, ChatMessage, ChatMessageCreate, ChatMessageResponse, ChatAdministrator, ChatAdministratorCreate, ChatAdministratorResponse
-from .contact import Contact, ContactCreate, ContactUpdate, ContactResponse, ContactList
+from .product import ProductCreate, ProductUpdate, ProductResponse, ProductList, ProductWithStats
+from .commentary import CommentaryCreate, CommentaryUpdate, CommentaryResponse
+from .like import LikeCreate, LikeResponse
+from .event import EventCreate, EventUpdate, EventResponse
+from .waiver import WaiverDataCreate, WaiverDataResponse, WaiverValidatorCreate, WaiverValidatorResponse
+from .chat import ChatRoomCreate, ChatRoomResponse, ChatMessageCreate, ChatMessageResponse, ChatAdministratorCreate, ChatAdministratorResponse
+from .contact import ContactCreate, ContactUpdate, ContactResponse, ContactList
 
 __all__ = [
     "UserCreate", "UserUpdate", "UserResponse", "UserLogin", "Token",
-    "Product", "ProductCreate", "ProductUpdate", "ProductResponse", "ProductList", "ProductWithStats",
-    "Commentary", "CommentaryCreate", "CommentaryUpdate", "CommentaryResponse",
-    "Like", "LikeCreate", "LikeResponse",
-    "Event", "EventCreate", "EventUpdate", "EventResponse",
-    "WaiverData", "WaiverDataCreate", "WaiverDataResponse",
-    "WaiverValidator", "WaiverValidatorCreate", "WaiverValidatorResponse",
-    "ChatRoom", "ChatRoomCreate", "ChatRoomResponse",
-    "ChatMessage", "ChatMessageCreate", "ChatMessageResponse",
-    "ChatAdministrator", "ChatAdministratorCreate", "ChatAdministratorResponse",
-    "Contact", "ContactCreate", "ContactUpdate", "ContactResponse", "ContactList"
+    "ProductCreate", "ProductUpdate", "ProductResponse", "ProductList", "ProductWithStats",
+    "CommentaryCreate", "CommentaryUpdate", "CommentaryResponse",
+    "LikeCreate", "LikeResponse",
+    "EventCreate", "EventUpdate", "EventResponse",
+    "WaiverDataCreate", "WaiverDataResponse",
+    "WaiverValidatorCreate", "WaiverValidatorResponse",
+    "ChatRoomCreate", "ChatRoomResponse",
+    "ChatMessageCreate", "ChatMessageResponse",
+    "ChatAdministratorCreate", "ChatAdministratorResponse",
+    "ContactCreate", "ContactUpdate", "ContactResponse", "ContactList"
 ] 
