1f24fd8bfbe5b1d2d3569a2b03a34c1c9c338042
Mario Roberto <gomez@thetecwave.com>
2025-08-05 21:51:44 -0600

    fix: Correct User import in schemas __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,4 @@
-from .user import User, UserCreate, UserUpdate, UserResponse
+from .user import UserCreate, UserUpdate, UserResponse, UserLogin, Token
 from .product import Product, ProductCreate, ProductUpdate, ProductResponse, ProductList, ProductWithStats
 from .commentary import Commentary, CommentaryCreate, CommentaryUpdate, CommentaryResponse
 from .like import Like, LikeCreate, LikeResponse
@@ -8,7 +8,7 @@ from .chat import ChatRoom, ChatRoomCreate, ChatRoomResponse, ChatMessage, ChatM
 from .contact import Contact, ContactCreate, ContactUpdate, ContactResponse, ContactList
 
 __all__ = [
-    "User", "UserCreate", "UserUpdate", "UserResponse",
+    "UserCreate", "UserUpdate", "UserResponse", "UserLogin", "Token",
     "Product", "ProductCreate", "ProductUpdate", "ProductResponse", "ProductList", "ProductWithStats",
     "Commentary", "CommentaryCreate", "CommentaryUpdate", "CommentaryResponse",
     "Like", "LikeCreate", "LikeResponse",
